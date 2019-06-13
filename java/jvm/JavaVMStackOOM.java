1d434982d6f91b7c4ec89fbf05424a829158fe8c
sunboyu <sunboyu@huashenghaoche.com>
2019-06-13 11:17:56 +0800

    创建线程导致内存溢出

---
+++ java/jvm/JavaVMStackOOM.java
@@ -0,0 +1,29 @@
+package jvm;
+
+/**
+ * @Date: 11:14 2019/6/13
+ * @Author: BoyuSun
+ * @Description: 创建线程导致内存溢出
+ * Java的线程在操作系统内核上工作，该代码可能会导致系统死机
+ */
+public class JavaVMStackOOM {
+    private void dontStop(){
+        while(true){}
+    }
+
+    public void stackLeakByThread(){
+        while(true){
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    dontStop();
+                }
+            }).start();
+        }
+    }
+
+    public static void main(String[] args) {
+        JavaVMStackOOM oom = new JavaVMStackOOM();
+        oom.stackLeakByThread();
+    }
+}
